d49e6718dfdc610292ab10a41e9d90c50f487cf8
Mohammed Labib Rafid Majumder <labib.rafid2003@gmail.com>
2024-12-09 21:49:18 +0600

    Up-to-date

---
+++ IntroductionTask/src/AreaOfEquilateralTriangle.java
@@ -0,0 +1,12 @@
+public class AreaOfEquilateralTriangle {
+    public static double calc(double arm){
+        return (Math.sqrt(3) / 4.00) * Math.pow(arm, 2);
+    }
+    public static void main(String args[]){
+        double arm = 10.5;
+        double area = calc(arm);
+
+        System.out.print("The Area of Equilateral Triangle is: " + area);
+
+    }
+}
