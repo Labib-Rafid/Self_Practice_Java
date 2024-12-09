39ae57ce3f23b115dfa62cccd09f1f3276f311e0
Mohammed Labib Rafid Majumder <labib.rafid2003@gmail.com>
2024-12-09 21:23:32 +0600

    Update

---
+++ IntroductionTask/src/AreaOfCircle.java
@@ -0,0 +1,8 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        double radius = 10;
+        double area = pi * (radius * radius);
+        System.out.println("The Area of Circle Is: " + area);
+    }
+}
---
+++ IntroductionTask/src/CelciusToFahrenheit.java
@@ -0,0 +1,7 @@
+public class CelciusToFahrenheit {
+    public static void main(String args[]){
+        double celciusTemp = 37.36;
+        double farenheitCalc = ((9.00 * celciusTemp) / 5.00) + 32;
+        System.out.println("The Farenheit Temperature is : " + farenheitCalc);
+    }
+}
